d249f3648cb3e35a73b7d85b1682ab7f1d57fc69
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-09-11 23:47:17 -0300

    feat: finish class 1

---
+++ mobile/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>RECEBA!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#101010",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    color: "#f4f9f6",
+    fontSize: 22,
+  },
+});
