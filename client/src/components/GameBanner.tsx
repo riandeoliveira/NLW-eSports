961a87f4a0ddfa52ee5c53b1c37c81115dc37a5e
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-09-17 15:12:54 -0300

    fix: fix styles

--- client/src/components/GameBanner.tsx
+++ client/src/components/GameBanner.tsx
@@ -7,7 +7,7 @@ interface GameBannerProps {
 export function GameBanner(props: GameBannerProps) {
   return (
     <a href="" className="relative rounded-lg overflow-hidden h-60">
-      <img src={props.bannerUrl} alt="" />
+      <img src={props.bannerUrl} alt="" className="w-full" />
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
         <strong className="font-bold text-white block">{props.title}</strong>
         <span className="text-zinc-300 text-sm block">
